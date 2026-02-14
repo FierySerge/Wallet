aa1fcafb86155141fcd4dab648ac692f985227d8
FierySerge <coolraddude123@gmail.com>
2026-02-13 20:30:15 -0500

    Delete sw.js

--- sw.js
+++
@@ -1,6 +0,0 @@
-self.addEventListener('install', (event) => {
-    console.log('Service worker installed');
-});
-self.addEventListener('fetch', (event) => {
-    // For now, just let all requests go to network
-});
